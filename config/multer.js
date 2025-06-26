ef5c60959b944ab98568aaef4e3468dcde57975f
rohitbansal2005 <rohitkumarbansal23@gmail.com>
2025-06-26 12:28:59 +0530

    add

--- config/multer.js
+++ config/multer.js
@@ -10,6 +10,9 @@ const storage = new CloudinaryStorage({
   }),
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+});
 
 module.exports = upload; 
